b42536d1bc64ce8d81aedd7eba7f7ce09282d2c1
henryofjupiter <oshardhenry@gmail.com>
2023-04-17 06:56:02 -0400

    refactors code

--- GroceryItems.h
+++ GroceryItems.h
@@ -17,9 +17,9 @@ public:
     void menuDisplay () const;
     void SetItemInput(string item) {this->itemInput = itemInput;}
     string converter (string item, int wordFreq);
-    void fileRead(string input);
+    void fileRead(string item);
     void fileWrite(string item, size_t wordFreq);
-    void mapping(map<string, int> &itemsInMap, string item, int wordFreq);
+    static void mapping(map<string, int> &itemsInMap, string item, int wordFreq);
     void printMap(map<string, int> &itemsInMap);
     void printMap(map<string, int> &itemsInMap, size_t menu);
 
