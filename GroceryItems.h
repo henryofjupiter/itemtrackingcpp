2443b150a9daa33de8022320486079528c1652f6
henryofjupiter <oshardhenry@gmail.com>
2023-04-17 04:04:25 -0400

    declares  & refactoring

--- GroceryItems.h
+++ GroceryItems.h
@@ -5,17 +5,27 @@
 #ifndef ITEMTRACKINGCPP_GROCERYITEMS_H
 #define ITEMTRACKINGCPP_GROCERYITEMS_H
 #include <string>
+#include <iostream>
+#include <map>
 #include <iomanip>
 
 using namespace std;
 
 class GroceryItems {
 public:
-    void menuLoop() const;
+    void menuLoop();
     void menuDisplay () const;
     void SetItemInput(string item) {this->itemInput = itemInput;}
+    string converter (string item, int wordFreq);
+    void fileRead(string input);
+    void fileWrite(string item, size_t wordFreq);
+    void mapping(map<string, int> &itemsInMap, string item, int wordFreq);
+    void printMap(map<string, int> &itemsInMap);
+    void printMap(map<string, int> &itemsInMap, size_t menu);
+
 private:
     string itemInput;
+    map<string, int> itemsInMap;
 
 };
 
