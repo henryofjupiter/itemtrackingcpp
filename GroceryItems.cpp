c99aee8269c386534bde8022b297985f97c930c3
henryofjupiter <oshardhenry@gmail.com>
2023-04-17 01:39:29 -0400

    adds function fileRead to read data from file adds function converter adds function mapping for storing data temporarily updates menuLoop & refactor

--- GroceryItems.cpp
+++ GroceryItems.cpp
@@ -5,11 +5,13 @@
 #include "GroceryItems.h"
 #include <string>
 #include <iostream>
+#include <fstream>
+#include <map>
 
 using namespace std;
 
 // menu loop handles decision and
-void GroceryItems::menuLoop() const {
+void GroceryItems::menuLoop() {
     string userInput;
 
     cout << "Enter number here: ";
@@ -20,6 +22,8 @@ void GroceryItems::menuLoop() const {
             //enter menu option one
             cout << "Enter item for search: ";
             cin >> userInput;
+            itemInput = userInput;
+            fileRead(userInput);
         }
         else if (userInput == "2") {
             //enter menu option two
@@ -28,10 +32,11 @@ void GroceryItems::menuLoop() const {
             //enter menu option three
         }
         else {
-            cout << "Input Error, Try Again" << endl;
             cout << endl;
+            cout << "Input Error, Try Again" << endl;
             menuDisplay();
         }
+        cout << "Enter number here: ";
         cin >> userInput;
     }
 }
@@ -43,4 +48,48 @@ void GroceryItems::menuDisplay() const {
     cout << "2. Print List of Items Purchased" << endl;
     cout << "3. Print Item Frequency" << endl;
     cout << "4. exit program" << endl;
+}
+
+// use user input to search file and output with frequency
+//of user input if word is found within file
+void GroceryItems::fileRead(string input) {
+    ifstream  inFS;
+    int wordFreq = 0;
+    string fileName;
+
+    cout << "Opening file..." << endl;
+    inFS.open("CS210_Project_Three_Input_File.txt");
+    if (!inFS.is_open()) {
+        cout << "could not open file" << endl;
+    }
+    while(!inFS.eof()) {
+        inFS >> fileName;
+
+        if (!inFS.fail()) {
+            // display user input and frequency
+            if(fileName == input) {
+                ++wordFreq;
+            }
+
+        }
+    }
+    cout << input << " " << wordFreq << endl;
+    mapping(input, wordFreq);
+    inFS.close();
+}
+
+// converts frequency of user search word found into '*'
+string GroceryItems::converter(string item, int wordFreq) {
+        string newString;
+
+        for (int i = 0; i < wordFreq; ++i) {
+            newString += '*';
+        }
+        return newString;
+}
+
+void GroceryItems::mapping(string input, int wordFreq) {
+    map<string, int> itemsInMap;
+
+    itemsInMap.emplace(input, wordFreq);
 }
