1406737fb625b6f576890e911fcab5489f9f5a5c
henryofjupiter <oshardhenry@gmail.com>
2023-04-16 23:15:08 -0400

    adds data member & functions

--- GroceryItems.h
+++ GroceryItems.h
@@ -4,12 +4,18 @@
 
 #ifndef ITEMTRACKINGCPP_GROCERYITEMS_H
 #define ITEMTRACKINGCPP_GROCERYITEMS_H
+#include <string>
+#include <iomanip>
 
+using namespace std;
 
 class GroceryItems {
 public:
+    void menuLoop() const;
+    void menuDisplay () const;
+    void SetItemInput(string item) {this->itemInput = itemInput;}
 private:
-
+    string itemInput;
 
 };
 
